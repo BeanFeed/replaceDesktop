46e87db85ff6366e0840ae1cd06c05c8e6b3714b
BeanFeed <86930720+BeanFeed@users.noreply.github.com>
2022-12-15 22:47:05 -0500

    Create replace.sh

---
+++ replace.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get upgrade
+apt-get install kde-plasma-desktop
+apt-get remove ubuntu-desktop
+apt-get autoremove
+apt-get remove nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot && sudo apt-get autoremove
